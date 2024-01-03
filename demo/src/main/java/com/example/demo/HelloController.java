e28bc2f59a2f67a66b4e8ff45a5acfcbe357f0c3
bitxuji <bitxuji@gmail.com>
2024-01-03 19:20:12 +0900

    RequestMapping to GetMapping

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -2,21 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String sayHello(Model model) {
         model.addAttribute("message", "Hello, Spring MVC!");
         return "hello";
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @GetMapping("/{name}")
     public String sayHelloToPerson(@PathVariable String name, Model model) {
         model.addAttribute("message", "Hello, " + name + "!");
         return "hello";
