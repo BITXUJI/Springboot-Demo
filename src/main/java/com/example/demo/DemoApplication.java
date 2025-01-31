cee524e886e3cb846208c7e5a8342f427c51a185
BITXUJI <bitxuji@gmail.com>
2025-02-01 00:38:08 +0900

    all checkstyle problem solved.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类.
+ * 
+ * @author Simon Gallagher
+ * @version 1.0
+ */
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    /**
+     * 启动方法.
+     * 
+     * @param args
+     *            命令行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 启动类.
+ * 
+ * @author Simon Gallagher
+ * @version 1.0
+ * @since 1.0
+ */
+package com.example.demo;
