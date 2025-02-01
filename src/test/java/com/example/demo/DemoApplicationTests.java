a7b59d5627d7c5256bdacb1b2528ead28a2ad108
BITXUJI <bitxuji@gmail.com>
2025-02-01 14:38:18 +0900

    verify the newest version of jacoco checkstyle pmd spotbugs sonarqube and some coding way

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class DemoApplicationTests {
--- src/test/java/com/example/demo/HelloControllerTest.java
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
- * HelloController 测试类
+ * HelloController 测试类.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,7 +22,7 @@ class HelloControllerTest {
     private MockMvc mockMvc;
 
     /**
-     * 测试访问 /hello
+     * 测试访问 /hello.
      * 预期返回状态码 200，视图名称为 "hello"，
      * 模型中包含 message="Hello, Spring MVC!"
      */
@@ -33,13 +35,13 @@ class HelloControllerTest {
     }
 
     /**
-     * 测试访问 /hello/{name}
+     * 测试访问 /hello/name.
      * 预期返回状态码 200，视图名称为 "hello"，
-     * 模型中包含 message="Hello, <name>!"
+     * 模型中包含 message="Hello, name!"
      */
     @Test
     void testSayHelloToPerson() throws Exception {
-        String name = "John";
+        final String name = "John";
         mockMvc.perform(get("/hello/" + name))
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"))
